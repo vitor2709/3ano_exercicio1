1292c0d3a41acbd4d942d33e3351fd10ab9b5426
Vitor2709 <vitor.3937@aluno.pr.senac.br>
2025-03-24 12:28:54 -0300

    site-beauty

---
+++ 3ano_exercico03/gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass:{
+            dist:{
+                options:{
+                    style: 'compressed'
+                },
+                files:{
+                    'dist/styles/main.css': 'src/styles/main.scss'
+                }
+            }
+        },
+        watch:{
+            sass:{
+                files:'src/styles/**/*.scss',
+                tasks:['sass']
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+}
